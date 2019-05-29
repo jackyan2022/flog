0e78a669a212b4307dbac8e9039d05146ae0a793
hebe889900 <jackyan63@gmail.com>
2019-05-28 22:32:45 -0400

    Added navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbdAccordionBasic } from './Accordion/accordion.basic';
+import { NavbarComponent } from './Navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgbdAccordionBasic
+    NgbdAccordionBasic,
+    NavbarComponent
   ],
   imports: [
     NgbModule,
